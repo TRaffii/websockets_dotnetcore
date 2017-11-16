a77ac0249f99c79619c35acf76114125a663aa06
Rafal Stepien <trafgraf@gmail.com>
2017-11-16 14:05:15 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,41 +33,9 @@ namespace WebSockets
 
             app.UseWebSockets();
             app.UseWebSocketEchoExtension();
-
-            // app.Use(async (context, next) =>
-            // {
-            //     if (context.Request.Path == "/ws")
-            //     {
-            //         if (context.WebSockets.IsWebSocketRequest)
-            //         {
-            //             WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-            //             await Echo(context, webSocket);
-            //         }
-            //         else
-            //         {
-            //             context.Response.StatusCode = 400;
-            //         }
-            //     }
-            //     else
-            //     {
-            //         await next();
-            //     }
-
-            // });
             app.UseFileServer();
         }
-          private async Task Echo(HttpContext context, WebSocket webSocket)
-        {
-            var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            while (!result.CloseStatus.HasValue)
-            {
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            }
-            await  webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-        }
+        
 
     }
 
